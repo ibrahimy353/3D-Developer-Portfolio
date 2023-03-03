c82fd6807266e55914a9b7972fc3eeae81c59be9
ibrahimy353 <ibrahimy353@gmail.com>
2023-03-03 23:09:35 +0300

    chhanges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,25 @@ import {navLinks} from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+
+  const [active, setfirst] = useState('')
+
+
   return (
-    <div>Navbar</div>
+    <nav className={'${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary'}>
+
+      <div className='w-full flex justify-between items-center max-w-7xl mx-aoto'>
+        <Link 
+         to= '/'
+         className='flex items-center gap-2'
+         onClick={() => {
+          setActive ('');
+         }}
+         >
+        
+        </Link>
+      </div>
+    </nav>
   )
 }
 
