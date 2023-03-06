b5e792b5e31e65f807351320975d329dbbb0c05b
ibrahimy353 <ibrahimy353@gmail.com>
2023-03-06 18:04:35 +0300

    working on hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,8 +24,7 @@ const Navbar = () => {
           >
           <img src={logo} alt='logo' className='w-9  h-9 object-contain'/>
           <p className= 'text-white text-[18px] font-bold cursor-pointer flex'>
-            Ibrahim 
-            <span className='sm:block hidden'>|
+            Ibrahim &nbsp;<span className='sm:block hidden'>|
             Portfolio </span>
           </p>
         </Link> 
