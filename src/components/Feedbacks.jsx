c59026eab17a3765f9845f3dd209a3103dbfdc3e
ibrahimy353 <ibrahimy353@gmail.com>
2023-03-08 23:02:57 +0300

    added images

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -13,11 +13,16 @@ const FeedbackCard =  ({index, testimonial, name, designation, company, image}) 
       <p className="text-white tracking-wider text-[18px]"> {testimonial} </p>  
       <div className="mt-7 flex justify-between items-center gap-1">
         <div className="flex-1 flex flex-col">
-          <p>
-            <span>@</span> {name}
+          <p className="text-white font-medium text-[16px]">
+            <span className="blue-text-gradient">@</span> {name}
           </p>
-            {designation} of {company}
+           <p className="mt-1 text-secondary text-[12px]">{designation} of {company}</p> 
         </div>
+
+      <img 
+        	src={image} 
+          alt={`feedback-by-${name}`}
+          className=" w-10 h-10 rounded-full object-cover"/>
       </div>
       </div>
     
