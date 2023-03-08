efc0d9ff6a8085d186133766fd5252a68276a7b9
ibrahimy353 <ibrahimy353@gmail.com>
2023-03-08 22:50:14 +0300

    comment section

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -4,6 +4,15 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
+const FeedbackCard =  ({index, testimonial, name, designation, company, image}) =>(
+  <motion.div 
+     variants={fadeIn("", "spring", index * 0.5, 0.75)}
+     className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full">
+    <p>{testimonial}</p>
+  </motion.div>
+
+)
+
 const Feedbacks = () => {
   return (
     <div className="mt-12 bg-black-100 rounded-[20px]">
